7ba89eb52b6ca111a659c6a65c7ded2cf29fb554
muhammad raffi <muhammadrf0306@gmail.com>
2026-08-20 14:52:47 +0700

    Configure Laravel for Vercel deployment v.006

--- api/index.php
+++ api/index.php
@@ -1,3 +1,33 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Konfigurasi direktori /tmp untuk Vercel Serverless
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath . '/framework/views', 0755, true);
+    mkdir($storagePath . '/framework/sessions', 0755, true);
+    mkdir($storagePath . '/framework/cache/data', 0755, true);
+    mkdir($storagePath . '/bootstrap/cache', 0755, true);
+    mkdir($storagePath . '/logs', 0755, true);
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Overwrite path storage dan bootstrap cache bawaan
+$app->useStoragePath($storagePath);
+$app->useBootstrapPath($storagePath . '/bootstrap');
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
