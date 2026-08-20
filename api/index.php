8d43f42e5e53da281f2e52f09e80d4f069435eba
muhammad raffi <muhammadrf0306@gmail.com>
2026-08-20 15:29:05 +0700

    Configure Laravel for Vercel deployment v.009

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Konfigurasi direktori /tmp untuk Vercel Serverless
+// Konfigurasi folder /tmp untuk Vercel
 $storagePath = '/tmp/storage';
 if (!is_dir($storagePath)) {
     mkdir($storagePath . '/framework/views', 0755, true);
@@ -18,9 +18,14 @@ if (!is_dir($storagePath)) {
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Overwrite path storage dan bootstrap cache bawaan
 $app->useStoragePath($storagePath);
 $app->useBootstrapPath($storagePath . '/bootstrap');
+
+// Set environment variable untuk menyimpan cache file di /tmp
+$_ENV['APP_CONFIG_CACHE'] = $storagePath . '/bootstrap/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $storagePath . '/bootstrap/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = $storagePath . '/bootstrap/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $storagePath . '/bootstrap/packages.php';
 
 $kernel = $app->make(Kernel::class);
 
